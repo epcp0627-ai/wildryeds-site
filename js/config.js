4a025fde3bbc018d0cb55aa612558a69547af934
epcp0627-ai <epcp0627@gmail.com>
2025-10-20 13:17:54 +0900

    set cognito user pool- 2

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-2_PVVrsp4Nf', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6att5qidr9gl6er3h6nlnlpll4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-northeast-2_BT0Id9kbC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7220vbebokq7qbrq9voof99orm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
